18f78a60d71ea674cf2e10e594370fb9d2a389f9
Dylan Taylor <aliendude5300@gmail.com>
2011-04-11 15:48:40 -0400

    working on making the background monitor update the user interface. currently causes problems, so it's disabled for now.

--- include/DeviceInformation.h
+++ include/DeviceInformation.h
@@ -6,44 +6,42 @@
 #include <libxml/tree.h>
 #include <libxml/parser.h>
 
-class DeviceInformation : public DeviceInfo
-{
-    public:
-        DeviceInformation(wxWindow* parent, wxString devName, uint16_t productID, obex_t *handle) : DeviceInfo(parent) { //TODO: get actual device status instead of faking it with hard-coded values
-            device_handle = handle;
-            char* s = smartpen_get_peninfo(device_handle);
-            printf("%s\n",s);
-            xmlDocPtr doc = xmlParseMemory(s, strlen(s));
-            xmlNode *cur = xmlDocGetRootElement(doc);
-            int batteryLevel = getBatteryRemaining(cur);
-            printf("batteryLevel: %d\n", batteryLevel);
-            int freeBytes = getFreeBytes(cur);
-            int totalBytes = getTotalBytes(cur);
-            char* fs;
-            sprintf(fs, "%d of %d bytes remaining.\n",freeBytes,totalBytes); //stores formatted string in fs
-            printf("%s",fs); //displays fs on stdout
-
-            deviceName->SetLabel(devName);
-            if (productID == LS_PULSE) {
-                deviceType->SetLabel(_("LiveScribe Pulse(TM) Smartpen"));
-            } else if (productID == LS_ECHO) {
-                deviceType->SetLabel(_("LiveScribe Echo(TM) Smartpen"));
-            } else {
-                deviceType->SetLabel(_("Unknown LiveScribe Device"));
-            }
-            batteryGauge->SetValue(batteryLevel);
-            wxString freeSpace(fs, wxConvUTF8);
-            storageRemaining->SetLabel(freeSpace);
-            smartpen_disconnect(device_handle);
-        };
-        virtual ~DeviceInformation();
-        int getBatteryRemaining(xmlNode *a_node);
-        float getBatteryVoltage(xmlNode *a_node);
-        long long int getFreeBytes(xmlNode *a_node);
-        long long int getTotalBytes(xmlNode *a_node);
-    protected:
-    private:
-        obex_t *device_handle;
+class DeviceInformation : public DeviceInfo {
+public:
+    DeviceInformation(wxWindow* parent, wxString devName, uint16_t productID, obex_t *handle) : DeviceInfo(parent) { //TODO: get actual device status instead of faking it with hard-coded values
+        device_handle = handle;
+        char* s = smartpen_get_peninfo(device_handle);
+        printf("%s\n",s);
+        xmlDocPtr doc = xmlParseMemory(s, strlen(s));
+        xmlNode *cur = xmlDocGetRootElement(doc);
+        int batteryLevel = getBatteryRemaining(cur);
+        printf("batteryLevel: %d\n", batteryLevel);
+        int freeBytes = getFreeBytes(cur);
+        int totalBytes = getTotalBytes(cur);
+        char* fs;
+        sprintf(fs, "%d of %d bytes remaining.\n",freeBytes,totalBytes); //stores formatted string in fs
+        printf("%s",fs); //displays fs on stdout
+        deviceName->SetLabel(devName);
+        if (productID == LS_PULSE) {
+            deviceType->SetLabel(_("LiveScribe Pulse(TM) Smartpen"));
+        } else if (productID == LS_ECHO) {
+            deviceType->SetLabel(_("LiveScribe Echo(TM) Smartpen"));
+        } else {
+            deviceType->SetLabel(_("Unknown LiveScribe Device"));
+        }
+        batteryGauge->SetValue(batteryLevel);
+        wxString freeSpace(fs, wxConvUTF8);
+        storageRemaining->SetLabel(freeSpace);
+        smartpen_disconnect(device_handle);
+    };
+    virtual ~DeviceInformation();
+    int getBatteryRemaining(xmlNode *a_node);
+    float getBatteryVoltage(xmlNode *a_node);
+    long long int getFreeBytes(xmlNode *a_node);
+    long long int getTotalBytes(xmlNode *a_node);
+protected:
+private:
+    obex_t *device_handle;
 };
 
 #endif // DEVICEINFORMATION_H
