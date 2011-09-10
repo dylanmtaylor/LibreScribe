0004461018cd2a254f6aa5b002a0efd45dff4ca5
Dylan Taylor <aliendude5300@gmail.com>
2011-09-10 14:51:17 -0400

    get rid of those annoying redefinition warnings during compilation

--- GUIFrame.cpp
+++ GUIFrame.cpp
@@ -14,6 +14,7 @@ You should have received a copy of the GNU General Public License
 along with LibreScribe.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "LibreScribe.h"
 #include "wx_pch.h"
 #include "GUIFrame.h"
 
