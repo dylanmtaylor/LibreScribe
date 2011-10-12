c5a951a8c73be3f7a7add41fd653b451bd3705a4
Dylan M. Taylor <dylan@dylanmtaylor.com>
2011-10-12 11:02:48 -0400

    rewriting generate-makefile.sh to use the new version of cbp2make

--- generate-makefile.sh
+++ generate-makefile.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-./cbp2make -in LibreScribe.cbp -out Makefile
+LONG_BIT=`getconf LONG_BIT`
+if [ $LONG_BIT -eq 64 ]; then
+   echo "64-bit operating system detected."
+   ./cbp2make.linux-x86_64 -in LibreScribe.cbp -out Makefile
+elif [ $LONG_BIT -eq 32 ]; then
+   echo "32-bit operating system detected."
+   ./cbp2make.linux-x86 -in LibreScribe.cbp -out Makefile
+else
+   echo "An unsupported operating system was detected. Aborting."
+fi
+
