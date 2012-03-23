3fc594d21ca78275a2e4b128cfc977da79792d5d
aliendude5300 <aliendude5300@gmail.com>
2012-03-22 23:52:32 -0400

    update project files to work with new layout

--- src/BackgroundMonitor.cc
+++ src/BackgroundMonitor.cc
@@ -14,7 +14,7 @@ You should have received a copy of the GNU General Public License
 along with LibreScribe.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "../GUIFrame.h"
+#include "GUIFrame.h"
 #include <string>
 
 struct udev *udev;
